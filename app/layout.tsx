4bc86e71928f0c4a54d32ca08337fc9b704996be
stephane.malho <stepahen.malho@gmail.com>
2023-10-08 18:47:31 +0200

    ✨ next-metadata : layout.tsx > add fonts and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,51 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import React from 'react'
- 
-export const metadata = {
-  title: 'Next.js 13 with Clerk',
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from 'next'
+
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: "--font-inter",
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: "--font-space-grotestk",
+})
+
+
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description: 'DevFlow is a platform for developers to share their ideas and projects',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  }
 }
- 
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={
+        {
+          elements : {
+            formButtonPrimary: 'primary-gradient',
+            footerActionLink: 'primary-text-gradient hover:text-primary-500',
+          }
+        }
+      }
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
