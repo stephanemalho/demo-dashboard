a94e03aa69fa36cb07e9f46b77c907be6be6f40f
stephane.malho <stepahen.malho@gmail.com>
2023-10-08 18:09:43 +0200

    ✨ add auth

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Ask question</div>
+  )
+}
+
+export default page
--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-  return (
-    <main><h1 className="h1-bold">Nextjs13 we&apos;re coming soon</h1></main>
-  )
-}
