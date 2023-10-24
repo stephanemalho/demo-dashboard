422f7cf03d840b30298e51d35392b2d8ea02e64f
stephaneMalho <stephane.malho@gmail.com>
2023-10-24 16:14:22 +0200

    ✨ ref/icons-sidebar > create context for toggeled sidebar

--- context/ScreenSizeContext.tsx
+++ context/ScreenSizeContext.tsx
@@ -2,39 +2,50 @@
 import React, { createContext, useState } from "react";
 
 interface SideBarContextProps {
-    toggleSideBar: boolean;
-    setToggleSideBar: (toggleSideBar: boolean) => void;
-    handleClick: () => void;
-};
+  toggleSideBar: boolean;
+  setToggleSideBar: (toggleSideBar: boolean) => void;
+  handleClick: () => void;
+  handleHover: () => void;
+}
 
-export const SideBarContext = createContext<SideBarContextProps | undefined>(undefined);
+export const SideBarContext = createContext<SideBarContextProps | undefined>(
+  undefined
+);
 
 export function SidebarProvider({ children }: { children: React.ReactNode }) {
-    
-    const [toggleSideBar, setToggleSideBar] = useState(false)  
+  const [toggleSideBar, setToggleSideBar] = useState(false);
 
-    const handleClick = () => {
-      setToggleSideBar(!toggleSideBar)
-      console.log("houhouhuouhouhuoh");
+  const handleClick = () => {
+    setToggleSideBar(!toggleSideBar);
+    console.log("houhouhuouhouhuoh");
+  };
+
+  const handleHover = () => {
+    if (toggleSideBar === true) {
+      setToggleSideBar(false);
     }
+  };
 
-    return (
-        <SideBarContext.Provider value={{
-            toggleSideBar,
-            setToggleSideBar,
-            handleClick
-        }}>
-            {children}
-        </SideBarContext.Provider>
-    );
+  return (
+    <SideBarContext.Provider
+      value={{
+        toggleSideBar,
+        setToggleSideBar,
+        handleClick,
+        handleHover,
+      }}
+    >
+      {children}
+    </SideBarContext.Provider>
+  );
 }
 
 export const useSidebar = () => {
-    const context = React.useContext(SideBarContext);
+  const context = React.useContext(SideBarContext);
 
-    if (context === undefined) {
-        throw new Error("useScreen must be used within a ScreenProvider");
-    }
+  if (context === undefined) {
+    throw new Error("useScreen must be used within a ScreenProvider");
+  }
 
-    return context;
+  return context;
 };
