9446473d3a6db9bd11b5f6530f86b80f15004586
stephaneMalho <stephane.malho@gmail.com>
2023-11-08 09:25:47 +0100

    ♻️ ref/7-nov-2023 > add bg-color to live container

--- components/shared/boxContainer/LiveContainer.tsx
+++ components/shared/boxContainer/LiveContainer.tsx
@@ -10,16 +10,16 @@ interface BoxLogsProps {
 }
 
 const LiveContainer = ({ children, logHeight, title, label }: BoxLogsProps) => {
- 
+
   return (
-    <div className="shadow-light100_dark100 w-full  bg-light-850 p-2">
-      <div className="base-bold flex items-center text-[12px] max-2xl:text-[8px]">
+    <div className="shadow-light100_dark100 w-full border-[#e4e4e4] bg-light-850">
+      <div className="base-bold flex h-[auto] items-center border-[1px] border-[#e4e4e4] bg-[#edf4ff] p-1 text-[12px] max-2xl:text-[8px]">
         <IconInfo title={title} />
         {label}
         <IconResize />
       </div>
       <div
-        className={`custom-scrollbar ${logHeight} overflow-y-auto border-t dark:border-slate-700`}
+        className={`custom-scrollbar ${logHeight} overflow-y-auto bg-[#fff] p-2`}
       >
         {children}
       </div>
