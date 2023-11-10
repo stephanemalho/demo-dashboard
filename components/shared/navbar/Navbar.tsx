260e584a627b3e4c54dc4fff04b21b4beebee1dd
stephaneMalho <stephane.malho@gmail.com>
2023-11-10 10:09:46 +0100

    💄 ✨ ref/7-nov-2023 > add carbon header color

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -10,7 +10,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-
   DropdownMenuTrigger,
 } from "@radix-ui/react-dropdown-menu";
 import ThreadsInfo from "../threadsInfo/ThreadsInfo";
@@ -19,18 +18,18 @@ const Navbar = () => {
   const { handleClick, toggleSideBar } = useSidebar(); // sideBar Context
 
   return (
-    <nav className="fixed z-10 flex h-[50px] w-full border-b-[1px] border-grey-border bg-[white]  shadow-light-300 ">
+    <nav className="fixed z-10 flex h-[50px] w-full border-b-[1px] border-[#000] bg-[#000]  shadow-light-300 ">
       <SignedIn>
         <div
-          className="flex h-[49px] w-[50px] cursor-pointer justify-center pb-1 hover:bg-[#f4f4f4] active:bg-[#e0e0e0]"
+          className="flex h-[49px] w-[50px] cursor-pointer justify-center hover:bg-[rgb(38,38,38)] active:border-[2px] active:border-[#fff]"
           onClick={handleClick}
         >
           {toggleSideBar ? (
             <Image
               src={"/assets/icons/close.svg"}
               alt={"menu"}
-              width={20}
-              height={20}
+              width={22}
+              height={22}
             />
           ) : (
             <Image
@@ -38,17 +37,17 @@ const Navbar = () => {
               alt={"menu"}
               width={20}
               height={20}
+              className="pb-1"
             />
           )}
         </div>
       </SignedIn>
-      <div className="flex h-[49px] w-[119px] hover:bg-[#f4f4f4] active:bg-[#e0e0e0]">
+      <div className="flex h-[49px] w-[119px] border-[2px] border-[#000] hover:bg-[#262626] active:border-[2px] active:border-[#fff]">
         <Link href="/" className="flex w-full items-center gap-2">
-          <span className="ml-3  dark:text-white">IBM</span>
-          <p className="h2-bold relative mr-2 w-full border-r-[1px] border-grey-border font-spaceGrotesk text-[12px] font-black text-dark-100">
-            <span className="text-[15px] italic text-logo-500">X</span>
-            Care
-          </p>
+          <span className="ml-3  text-white">IBM</span>
+          <h2 className="mr-2 w-full border-r-[1px] border-grey-border font-bold  text-white">
+            X-Care
+          </h2>
         </Link>
       </div>
       <SignedIn>
@@ -60,7 +59,7 @@ const Navbar = () => {
       <div className="flex-between ml-auto">
         <SignedOut>
           <div
-            className="flex h-[49px] w-[50px] cursor-pointer justify-center pb-1 hover:bg-[#f4f4f4] active:bg-[#e0e0e0]"
+            className="flex h-[49px] w-[50px] cursor-pointer justify-center pb-1 hover:bg-[#262626] active:border-[2px] active:border-[#fff]"
             onClick={handleClick}
           >
             <DropdownMenu>
@@ -75,7 +74,7 @@ const Navbar = () => {
               <DropdownMenuContent>
                 <DropdownMenuItem>
                   <Link href="/sign-in">
-                    <Button className="h-[49px] w-full rounded-none border-[1px] border-slate-900 bg-white hover:bg-[#f4f4f4]">
+                    <Button className="h-[49px] w-full rounded-none border-[1px] border-slate-900 bg-white hover:bg-[#262626]">
                       <span className="font-semibold text-[#0f62fe]">
                         Log In
                       </span>
@@ -96,7 +95,7 @@ const Navbar = () => {
         </SignedOut>
         {/* <Theme /> */}
         <SignedIn>
-          <div className="flex h-[49px] w-[50px] cursor-pointer items-center justify-center hover:bg-[#f4f4f4] active:bg-[#e0e0e0]">
+          <div className="flex h-[49px] w-[50px] cursor-pointer items-center justify-center hover:bg-[#262626] active:border-[2px] active:border-[#fff]">
             <UserButton
               afterSignOutUrl="/"
               appearance={{
@@ -109,7 +108,7 @@ const Navbar = () => {
               }}
             />
           </div>
-          <div className="flex h-[49px] w-[50px] cursor-pointer justify-center pb-1 hover:bg-[#f4f4f4] active:bg-[#e0e0e0]">
+          <div className="flex h-[49px] w-[50px] cursor-pointer justify-center hover:bg-[#262626] active:border-[2px] active:border-[#fff]">
             <Image
               src={"/assets/icons/menu-left.svg"}
               alt={"menu"}
--- components/shared/threadsInfo/MainInfo.tsx
+++ components/shared/threadsInfo/MainInfo.tsx
@@ -9,14 +9,14 @@ const MainInfo = ({ infoTitle, data }: MainInfoProps) => {
   return (
     <div
       title={infoTitle}
-      className="flex h-[49px] w-[100px] flex-col justify-center rounded-none  px-2 text-[0.4rem] hover:bg-[#f4f4f4] "
+      className="flex h-[49px] w-[105px] flex-col justify-center rounded-none  border-[2px] border-[#000] px-2 text-[0.4rem] hover:bg-[#262626] active:border-[2px] "
     >
       <div className="border-l-[1px] border-grey-border pl-2">
-        <p className=" text-[10px] text-gray-600 max-2xl:text-[0.6rem]">
+        <p className=" text-[10px] text-gray-300 max-2xl:text-[0.6rem]">
           {" "}
           {infoTitle}
         </p>
-        <p className="text-xs font-bold text-gray-900 max-2xl:text-[0.7rem]">
+        <p className="text-xs font-bold text-[#fff] max-2xl:text-[0.7rem]">
           {data}
         </p>
       </div>
