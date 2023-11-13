456b4e1c66ac2278c3ddc805125cef220d01176b
stephaneMalho <stephane.malho@gmail.com>
2023-11-13 13:45:50 +0100

    💄 theme4 > add hover in wrapIcon #63D1D4

--- components/WrapIcon.tsx
+++ components/WrapIcon.tsx
@@ -6,7 +6,7 @@ interface WrapIconProps {
 
 const WrapIcon = ({ children } : WrapIconProps) => {
   return (
-    <div className="flex h-[30px] w-[30px] cursor-pointer items-center hover:bg-[#9ad1a0]">
+    <div className="flex h-[30px] w-[30px] cursor-pointer items-center hover:bg-[#90d7da]">
       {children}
     </div>
   )
