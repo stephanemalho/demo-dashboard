e2c49794154c85be8208251b6b58786f127f28ae
stephaneMalho <stephane.malho@gmail.com>
2023-11-20 14:42:47 +0100

    🚧  ref > remove chore page tests

--- app/(root)/versionning/chore/page.tsx
+++ app/(root)/versionning/chore/page.tsx
@@ -1,51 +1,11 @@
-import { dashboard } from "@/data/dashboard";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import moment from "moment";
-import TableRows from "@/components/shared/logBox/logs-components/TableRows";
-
+import React from 'react'
 
 const page = () => {
-  const data = dashboard;
-  const logs = data.Logs;
-
   return (
-    <div className="m-auto flex h-[600px] w-[80vw] overflow-hidden ">
-    <Table className="z-[1] text-[0.7rem] max-2xl:text-[0.5rem]">
-      <TableHeader className="sticky top-0">
-        <TableRows className="bg-[#e2e2e2]" />
-      </TableHeader>
-      <TableBody className="code-font ">
-        {Object.keys(logs).map((logKey) => {
-          const logEntry = logs[logKey as unknown as keyof typeof logs];
-          const formattedTimestamp = moment(logEntry.TimeStamp).format(
-            "h:mm:ss a, MMMM"
-          );
-
-          return (
-            <TableRow key={logKey} className="font-bold hover:bg-[#E0E0E1]">
-              <TableCell title="ThreadId">{logEntry.ThreadID}</TableCell>
-              <TableCell title="SessionID">{logEntry.SessionID}</TableCell>
-              <TableCell title="Level">{logEntry.Level}</TableCell>
-              <TableCell className="max-w-[80px] truncate" title="TimeStamp">
-                {formattedTimestamp}
-              </TableCell>
-              <TableCell title="Logger">{logEntry.Logger}</TableCell>
-              <TableCell className="w-[50%] truncate" title="Message">
-                {logEntry.Message}
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+    <div className="slide-in-right text-dark500_light700 flex w-full flex-col-reverse justify-between gap-4 sm:flex-row">
+      <h1 className="h1-bold">Versionning chore</h1>
     </div>
-  );
-};
+  )
+}
 
-export default page;
+export default page
