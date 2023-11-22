ce94b5482ed51659ee9bb183be6bd21d57def006
stephaneMalho <stephane.malho@gmail.com>
2023-11-22 17:46:17 +0100

    🔧 add rux file in next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Ajoute une règle pour les fichiers .rux
+    config.module.rules.push({
+      test: /\.rux$/,
+      type: 'asset/source',
+    });
+
+    // Important : retourne la configuration modifiée
+    return config;
+  },
+};
+
+module.exports = nextConfig;
