bcc6f97b1e615bc65f2455505e7a466208a796af
stephaneMalho <stephane.malho@gmail.com>
2023-12-11 10:23:39 +0100

    ✨ read pathname to make conditional render in RightSideBar

--- components/shared/sideBars/RightSideBar.tsx
+++ components/shared/sideBars/RightSideBar.tsx
@@ -1,18 +1,40 @@
 "use client";
+import { usePathname, useSearchParams } from 'next/navigation'
 import { useSidebar } from "@/context/SideBarProvider";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import MetaData from "../MetaData";
 
 
 const RightSideBar = () => {
+  const [isMetaVisible, setIsMetaVisible] = useState(false);
+  const { toggleRightSideBar } = useSidebar(); // sideBar Context
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+ 
+  useEffect(() => {
+    const url = `${pathname}?${searchParams}`
+    console.log(url)
+    
+    if (url.includes("statistics/ressources")) {
+      setIsMetaVisible(true);
+    } else {
+      setIsMetaVisible(false);
+    }
 
-  const {toggleRightSideBar} = useSidebar(); // sideBar Context
+  }, [pathname, searchParams]);
+
+  
 
   if (!toggleRightSideBar) {
     return null; // don't render anything if the sidebar is closed
   }
   return (
-    <section className={`${toggleRightSideBar ? "slide-in-rightSidebar" : ""}  custom-scrollbar fixed right-0 top-[50px] z-50 flex h-screen w-[650px] flex-col overflow-y-auto  bg-[rgba(0,0,0,0.7)] p-6 pt-36 dark:shadow-none max-xl:hidden lg:w-[40vw]`}>
-     
+    <section
+      className={`${
+        toggleRightSideBar ? "slide-in-rightSidebar" : ""
+      }  custom-scrollbar fixed right-0 top-[50px] z-50 flex h-screen w-[650px] flex-col overflow-y-auto  bg-[rgba(0,0,0,0.7)] p-6 pt-36 dark:shadow-none max-xl:hidden lg:w-[40vw]`}
+    >
+      {isMetaVisible && <MetaData />}{" "}
     </section>
   );
 };
