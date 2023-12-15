9989fc860dc1196b5ba3578759fd0fa082dd4db3
stephaneMalho <stephane.malho@gmail.com>
2023-12-15 10:52:38 +0100

    ✨ Fix DiffTable style on small screen

--- app/(root)/live-delivery/create/Diff/DiffLines.tsx
+++ app/(root)/live-delivery/create/Diff/DiffLines.tsx
@@ -5,7 +5,7 @@ import RenderDiffLines from "./DiffComponent/RenderDiffLines";
 
 const DiffLines: React.FC<DiffViewerProps> = ({ oldText, newText }) => {
   return (
-    <div className="h-[85vh] w-full max-2xl:h-[70vh]">
+    <div className="h-[85vh] w-full max-2xl:h-[76vh]">
       <div className="flex h-full flex-col border-[1px]">
         <div className={`flex h-full w-full overflow-auto`}>
           <div className="mx-auto h-full w-full">
