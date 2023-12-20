5c05c777a997db4be7a6e7739f77f62048db9d8e
stephaneMalho <stephane.malho@gmail.com>
2023-12-20 18:14:11 +0100

    ♻️ add ContentContainer & LiveContainer to versionning pages

--- app/(root)/versionning/chore/page.tsx
+++ app/(root)/versionning/chore/page.tsx
@@ -1,13 +1,19 @@
+import ContentContainer from "@/components/shared/contentContainer/ContentContainer";
 import HeadVersionningChores from "./choreComponents/HeadVersionningChores";
+import LiveContainer from "@/components/shared/boxContainer/LiveContainer";
 
 export default function page() {
 
   return (
-    <div className="flex h-screen bg-gray-100">
-      <div className="flex-1 p-8">
+   <ContentContainer>
+      <LiveContainer
+        title="Versionning Chores"
+        label="Versionning Chores"
+        logHeight="h-96"
+      >
         <HeadVersionningChores />
         <div className="h-full bg-white" />
-      </div>
-    </div>
+      </LiveContainer>
+   </ContentContainer>
   )
 }
