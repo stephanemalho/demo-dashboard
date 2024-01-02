a592e569c81aae4549277cc116bad5821567bdeb
stephaneMalho <stephane.malho@gmail.com>
2024-01-02 12:00:09 +0100

    ♻️ remove old comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,11 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
-// import { IBM_Plex_Sans as IbmPlexSans } from "next/font/google";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/context/ThemeProvider";
 import { FullscreenProvider } from "@/context/ScreenSizeProvider";
 
 import "./globals.css";
 import { SidebarProvider } from "@/context/SideBarProvider";
-// import localFont from 'next/font/local'
- 
-// Font files can be colocated inside of `pages`
-
-// const nowFont = localFont({
-//   src: [
-//     {
-//       path: '/assets/fonts/aexis-font/Now-Bold.otf',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//   ],
-// })
-
-
-// const ibmPlexSans = IbmPlexSans({
-//   subsets: ["latin"],
-//   weight: ["100", "200", "400", "700"],
-//   variable: "--font-ibm-plex-sans",
-// });
 
 export const metadata: Metadata = {
   title: "X-CARE",
@@ -35,8 +14,6 @@ export const metadata: Metadata = {
     icon: "/assets/images/aexis.png",
   },
 };
-
-// ${ibmPlexSans.variable} 
 
 export default function RootLayout({
   children,
