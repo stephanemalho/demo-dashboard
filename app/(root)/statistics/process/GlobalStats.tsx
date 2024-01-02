c2316b39820f484fe8604c8bb96164a19ed28334
stephaneMalho <stephane.malho@gmail.com>
2024-01-02 08:44:41 +0100

    ♻️  fix canvas layout for big sizes

--- app/(root)/statistics/process/GlobalStats.tsx
+++ app/(root)/statistics/process/GlobalStats.tsx
@@ -4,10 +4,10 @@ import chartConfig from "./ChartsCanvas/ChartConfig";
 
 const GlobalStats = () => {
   return (
-    <div className="grid grid-cols-1 gap-8 p-1 md:grid-cols-2">
+    <div className="grid grid-cols-1 gap-8 p-1 md:grid-cols-2 lg:grid-rows-2">
       {chartConfig.map((chart, index) => (
-        <div key={index} className="rounded-lg p-3 shadow-lg">
-          <div className="chart-container h-[30vh]">
+        <div key={index} className="rounded-lg p-3 shadow-lg lg:min-h-[40vh]">
+          <div className="chart-container max-2xl:h-[30vh] lg:h-full">
             <chart.Component {...chart.props} />
           </div>
         </div>
