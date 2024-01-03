1b39b173e05ec3d6577e3cdd887bda54099c8da2
stephaneMalho <stephane.malho@gmail.com>
2024-01-03 16:17:14 +0100

    ✨ change VersioningRule using DataTable cpnt

--- api/versioning/getVersioning.ts
+++ api/versioning/getVersioning.ts
@@ -1,8 +1,11 @@
 
 import { VersioningChoreEntry } from "@/app/(root)/versionning/chore/choreComponents/columns";
 import { VersioningProcessEntry } from "@/app/(root)/versionning/process/processComponents/columns";
+import { VersioningRuleEntry } from "@/app/(root)/versionning/rule-page/ruleComponents/columns";
+
 import versioningChoreData from "@/data/versioning/chore.json";
 import versioningProcessData from "@/data/versioning/process.json";
+import versioningRuleData from "@/data/versioning/rule.json";
 
 
 export async function getVersioningChoreData(): Promise<VersioningChoreEntry[]> {
@@ -11,4 +14,8 @@ export async function getVersioningChoreData(): Promise<VersioningChoreEntry[]> 
 
 export async function getVersioningProcessData(): Promise<VersioningProcessEntry[]> {
   return versioningProcessData ;
+}
+
+export async function getVersioningRuleData(): Promise<VersioningRuleEntry[]> {
+  return versioningRuleData ;
 }
--- app/(root)/versionning/rule-page/page.tsx
+++ app/(root)/versionning/rule-page/page.tsx
@@ -1,24 +1,11 @@
 import ContentContainer from "@/components/shared/contentContainer/ContentContainer";
-import SearchRules from "./ruleComponents/SearchRules";
-import LiveContainer from "@/components/shared/boxContainer/LiveContainer";
+import VersioningRuleTable from "./ruleComponents/VersioningRuleTable";
+
 
 export default function page() {
   return (
     <ContentContainer>
-      <LiveContainer title="Rules" label="Rules" logHeight="h-96">
-        <div className="flex justify-between">
-          <SearchRules />
-          <div className="flex space-x-4">
-            <button className="rounded-none border border-gray-300 bg-transparent px-4 py-2 font-medium text-sm text-gray-900 hover:bg-gray-50 focus:ring-4 focus:ring-gray-200">
-              Info
-            </button>
-            <button className="rounded-none border border-gray-300 bg-transparent px-4 py-2 font-medium text-sm text-gray-900 hover:bg-gray-50 focus:ring-4 focus:ring-gray-200">
-              Rules
-            </button>
-          </div>
-        </div>
-        <div className="h-full bg-white" />
-      </LiveContainer>
+      <VersioningRuleTable />
     </ContentContainer>
   );
 }
