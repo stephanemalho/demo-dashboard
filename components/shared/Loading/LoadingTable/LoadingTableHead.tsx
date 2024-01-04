9e99af8593efebd5d364d53c2d37ce7232c1263f
stephaneMalho <stephane.malho@gmail.com>
2024-01-04 15:41:09 +0100

    ♻️ manage LoadingTable into small components

---
+++ components/shared/Loading/LoadingTable/LoadingTableHead.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoadingTableHead = () => {
+  return (
+    <div className="sticky top-0 bg-gray-100">
+      <div className=" h-[25px] bg-gray-200" />
+      <div className="h-[20px]  bg-gray-300" />
+    </div>
+  );
+};
+
+export default LoadingTableHead;
