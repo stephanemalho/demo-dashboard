094ff6b2d9f69e99763f68ee4501c1299ecb61c7
stephaneMalho <stephane.malho@gmail.com>
2024-01-08 14:26:43 +0100

    ✨ logs-component add FilterButton

--- app/(root)/dashboard/logs-components/columns.tsx
+++ app/(root)/dashboard/logs-components/columns.tsx
@@ -1,27 +1,85 @@
+import FilterBouton from "@/components/shared/tableThreads/FilterBouton";
 import { formatLogDate } from "@/lib/utils/date";
 import { ColumnDef } from "@tanstack/react-table";
 
 export interface DashboardLogEntry {
-      ID: string,
-      ThreadID: string,
-      SessionID: string,
-      Level: string,
-      TimeStamp: string,
-      Logger: string,
-      Message: string,
+  ID: string;
+  ThreadID: string;
+  SessionID: string;
+  Level: string;
+  TimeStamp: string;
+  Logger: string;
+  Message: string;
 }
 
 export const columns: ColumnDef<DashboardLogEntry>[] = [
   // Définissez ici les colonnes en fonction de la structure de jsonData
-  { accessorKey: "ThreadID", header: "ThreadID" },
-  { accessorKey: "SessionID", header: "SessionID" },
-  { accessorKey: "Level", header: "Level" },
-  { accessorKey: "TimeStamp", header: "TimeStamp",
-  cell: (info) => {
-    const formattedTimestamp = formatLogDate(info);
-    return (<span>{formattedTimestamp}</span>);
-  }
+  {
+    accessorKey: "ThreadID",
+    header: ({ column }) => {
+      return (
+        <FilterBouton
+          minSize="min-w-[100px]"
+          column={column}
+          label={"ThreadID"}
+        />
+      );
+    },
+  },
+  { accessorKey: "SessionID", header: ({ column }) => {
+    return (
+      <FilterBouton
+        minSize="min-w-[100px]"
+        column={column}
+        label={"SessionID"}
+      />
+    );
+  },
 },
-  { accessorKey: "Logger", header: "Logger" },
-  { accessorKey: "Message", header: "Message" },
+  { accessorKey: "Level", header: ({ column }) => {
+    return (
+      <FilterBouton
+        minSize="min-w-[100px]"
+        column={column}
+        label={"Level"}
+      />
+    );
+  },
+},
+  {
+    accessorKey: "TimeStamp",
+    header: ({ column }) => {
+      return (
+        <FilterBouton
+          minSize="min-w-[100px]"
+          column={column}
+          label={"TimeStamp"}
+        />
+      );
+    },
+    cell: (info) => {
+      const formattedTimestamp = formatLogDate(info);
+      return <span>{formattedTimestamp}</span>;
+    },
+  },
+  { accessorKey: "Logger", header: ({ column }) => {
+    return (
+      <FilterBouton
+        minSize="min-w-[100px]"
+        column={column}
+        label={"Logger"}
+      />
+    );
+  },
+},
+  { accessorKey: "Message", header: ({ column }) => {
+    return (
+      <FilterBouton
+        minSize="min-w-[100px]"
+        column={column}
+        label={"Message"}
+      />
+    );
+  },
+},
 ];
